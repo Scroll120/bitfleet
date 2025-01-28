afb632442c8e96d7241452d47c8163f03c9f65b1
KocsisAGyorgy <gurikocsis31@gmail.com>
2025-01-28 16:31:04 +0100

    created backend files with springboot

---
+++ Bitfleet/backend/src/main/java/com/codecool/bitfleet/BitfleetApplication.java
@@ -0,0 +1,13 @@
+package com.codecool.bitfleet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BitfleetApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BitfleetApplication.class, args);
+    }
+
+}
---
+++ Bitfleet/backend/src/test/java/com/codecool/bitfleet/BitfleetApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codecool.bitfleet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BitfleetApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
