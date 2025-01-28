07bcf07b6522fc44ce5a3360d52b34dbef391309
KocsisAGyorgy <gurikocsis31@gmail.com>
2025-01-28 16:57:51 +0100

    added frontend react files, router and simple navbar

---
+++ Bitfleet/frontend/src/components/NavigationBar/NavigationBar.jsx
@@ -0,0 +1,11 @@
+import './NavigationBar.css';
+
+export default function NavigationBar () {
+
+
+    return (
+        <div>
+            <p>Navbar</p>
+        </div>
+    )
+}
---
+++ Bitfleet/frontend/src/pages/homePage/HomePage.jsx
@@ -0,0 +1,10 @@
+import "./homePage.css";
+
+export default function HomePage() {
+
+    return (
+        <div>
+            <p>Bitfleet</p>
+        </div>
+    )
+}
